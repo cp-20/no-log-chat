dff4efe8fdaa549efb9e5fb042fbbabef1d48914
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-01-05 19:17:06 +0900

    ➕ add Mantine UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import 'ress';
 import '@/styles/global.scss';
+import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </>
   );
 }
