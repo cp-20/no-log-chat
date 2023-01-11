4592606fd5968e25f059d6a4e9a97aecff3043b7
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-01-11 13:55:03 +0900

    ✨ サーバーからpingが返ってこなかった時に自動で再接続するように

--- src/lib/chat.ts
+++ src/lib/chat.ts
@@ -1,6 +1,6 @@
 import { useAtom } from 'jotai';
 import { atom } from 'jotai';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { timeline, TimelineAtom } from '@/atoms/timeline';
 import { useUsernameAtom } from '@/atoms/username';
 import { useNotification } from '@/lib/notification';
@@ -13,6 +13,7 @@ export const useChat = () => {
   const [socket, setSocket] = useAtom(socketAtom);
   const [_, setTimeline] = useAtom(TimelineAtom);
   const username = useUsernameAtom();
+  const ping = useRef(true);
 
   const addTimeline = useCallback(
     (data: timeline) => {
@@ -22,27 +23,20 @@ export const useChat = () => {
     [notify, setTimeline],
   );
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      socket?.send(
-        JSON.stringify({
-          type: 'ping',
-        }),
-      );
-    }, 60 * 1000);
+  const sendJoinMessage = useCallback((username: string, socket: WebSocket) => {
+    socket?.send(
+      JSON.stringify({
+        type: 'join',
+        data: {
+          author: username,
+          ts: Date.now(),
+        },
+      }),
+    );
+  }, []);
 
-    return () => clearInterval(interval);
-  }, [socket]);
-
-  useEffect(() => {
-    setSocket((socket) => {
-      if (socket) return socket;
-      return new WebSocket(process.env.NEXT_PUBLIC_API_SERVER as string);
-    });
-  }, [setSocket]);
-
-  useEffect(() => {
-    if (socket) {
+  const socketHandler = useCallback(
+    (socket: WebSocket) => {
       socket.onmessage = (res) => {
         const payload = JSON.parse(res.data);
         if (payload.type === 'message') {
@@ -62,9 +56,67 @@ export const useChat = () => {
             ts: payload.data.ts,
           });
         }
+        if (payload.type === 'pong') {
+          ping.current = false;
+        }
       };
-    }
-  }, [addTimeline, socket]);
+
+      socket.onopen = () => {
+        console.log('connected', username);
+
+        if (username) {
+          sendJoinMessage(username, socket);
+        }
+      };
+    },
+    [addTimeline, sendJoinMessage, username],
+  );
+
+  const setupSocket = useCallback(() => {
+    setSocket((socket) => {
+      if (socket && socket.readyState !== socket.CLOSED) return socket;
+
+      const newSocket = new WebSocket(
+        process.env.NEXT_PUBLIC_API_SERVER as string,
+      );
+
+      socketHandler(newSocket);
+
+      return newSocket;
+    });
+  }, [setSocket, socketHandler]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // 前のpingが返ってきてなければ
+      if (ping.current) {
+        addTimeline({
+          author: 'システム',
+          text: `エラーが発生しました。再接続を試みています...`,
+          ts: Date.now(),
+        });
+
+        socket?.close();
+        setupSocket();
+        ping.current = false;
+
+        return;
+      }
+
+      ping.current = true;
+      socket?.send(
+        JSON.stringify({
+          type: 'ping',
+        }),
+      );
+    }, 3 * 1000); // 3s
+
+    return () => clearInterval(interval);
+  }, [addTimeline, setupSocket, socket]);
+
+  useEffect(() => {
+    setupSocket();
+  }, [setupSocket]);
 
   const sendMessage = (message: string) => {
     socket?.send(
@@ -79,17 +131,14 @@ export const useChat = () => {
     );
   };
 
-  const join = (username: string) => {
-    socket?.send(
-      JSON.stringify({
-        type: 'join',
-        data: {
-          author: username,
-          ts: Date.now(),
-        },
-      }),
-    );
-  };
+  const join = useCallback(
+    (username: string) => {
+      if (socket) {
+        sendJoinMessage(username, socket);
+      }
+    },
+    [sendJoinMessage, socket],
+  );
 
   return { sendMessage, join };
 };
