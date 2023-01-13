5d2c2f08d2231569879af1375f111c701029c8cd
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-01-13 12:22:25 +0900

    🚧 参加するまではメンバー数の表示をなくすように

--- src/components/MemberList/MemberList.tsx
+++ src/components/MemberList/MemberList.tsx
@@ -4,10 +4,14 @@ import Styles from '@/components/MemberList/member-list.module.scss';
 
 export const MemberList: React.FC = () => {
   const { members } = useMembersAtom();
-  return (
-    <div className={Styles['member-list']}>
-      <IconUsers className={Styles['member-list-icon']} />
-      <span className={Styles['member-list-counter']}>{members.length}</span>
-    </div>
-  );
+  if (members !== null) {
+    return (
+      <div className={Styles['member-list']}>
+        <IconUsers className={Styles['member-list-icon']} />
+        <span className={Styles['member-list-counter']}>{members.length}</span>
+      </div>
+    );
+  }
+
+  return <></>;
 };
